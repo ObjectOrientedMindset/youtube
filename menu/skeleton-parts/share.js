894fe25601cfdbb610953847bb93e306e90cbb9a
Mithat Ucar <mithat4226@hotmail.com>
2024-05-03 01:54:40 +0300

    share_button

---
+++ menu/skeleton-parts/share.js
@@ -0,0 +1,33 @@
+/*--------------------------------------------------------------
+>>> SHARE (No Functionality)
+--------------------------------------------------------------*/
+
+extension.skeleton.main.layers.section.share = {
+  component: "button",
+  variant: "share",
+  category: true,
+  icon: {
+    component: "span",
+    svg: {
+      component: "svg",
+      attr: {
+        viewBox: "0 0 24 24",
+        fill: "none",
+        stroke: "currentColor",
+        "stroke-linecap": "round",
+        "stroke-linejoin": "round",
+        "stroke-width": "2",
+      },
+      path: {
+        component: "path",
+        attr: {
+          d: "M18 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0zm-12 7a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm12 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM8.59 13.51a9 9 0 0 1 13.42-3.98l-6.87 4.98-6.55-2.65a9 9 0 0 1 0 1.65z",
+        },
+      },
+    },
+  },
+  label: {
+    component: "span",
+    text: "Share",
+  },
+};
